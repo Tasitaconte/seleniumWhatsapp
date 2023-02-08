8dd3c39dd53d1050cd28b4dfb2dccfa40799cdac
tasitaconte <thomasmauricio521@gmail.com>
2023-02-08 13:55:51 -0500

    arreglo bugs en el envio de mensaje

--- index.py
+++ index.py
@@ -14,38 +14,24 @@ PATH = "C:/Users/thoma/OneDrive/Escritorio/SeleniumPythonTwitterClaseFESC/chrome
 rutaMsm = '//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]'
 URL = "https://web.whatsapp.com/"
 
-new = '//*[@id="pane-side"]/div[1]/div/div/div[1]'
 # Ruta para mensajes whatsapp
-rutaMsmdos = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
+canvaWhat = '//*[@id="app"]/div/div/div[3]/div[1]/div/div/div[2]/div/canvas'
+rutaMsmdos = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p'
 usuario = '//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div/div[2]/div[1]/div[1]/span'
 
 driver = webdriver.Chrome(PATH)
-canvaWhat = '//*[@id="app"]/div/div/div[3]/div[1]/div/div/div[2]/div/canvas'
-
-
-def seleccionChat(nombre: str):
-    print("Buscando el chat wait moment")
-    chatElemnt = driver.find_elements(By.TAG_NAME, "span")
-    for chat in chatElemnt:
-        if chat.text == nombre:
-            print("te encontre esponja")
-            chat.click()
-            return True
-    return False
-
 
 def seleccionBusqueda(nombre: str):
     buscarChat = driver.find_element(By.XPATH, rutaMsm)
     buscarChat.send_keys(nombre)
     buscarChat.send_keys(Keys.ENTER)
+    buscarChat.clear()
     return True
-
 
 def enviarMensaje(mensaje: str):
     boxChat = driver.find_element(By.XPATH, rutaMsmdos)
     boxChat.send_keys(mensaje)
     boxChat.send_keys(Keys.ENTER)
-
 
 def inicioWhat():
     try:
@@ -53,10 +39,8 @@ def inicioWhat():
     except:
         return True
     return False
-
-
+    
 def main():
-
     driver.get(URL)
     print("Escane QR")
     time.sleep(5)
@@ -67,25 +51,21 @@ def main():
             registrar = input()
 
             print("Escriba su mensaje")
-            mensaje = input()
+            mensaje = input()            
             
             print("cuantas veces vas a mandar el mensaje")
             num = int(input())
 
             if (seleccionBusqueda(registrar)):
+                time.sleep(2)
                 for i in range(num):
                     enviarMensaje(mensaje)
                 print("Mensaje enviado")
-            else:
-                print("contacto no encontrado")
 
             print("Quiere salir? Y / sino pulse cualquier tecla")
             opcion = input()
             if (opcion == 'y' or opcion == 'Y'):
                 return False
-
-
     driver.close()
-
 
 main()
